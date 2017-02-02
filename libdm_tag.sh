0cb316d737d1aa48a9d3031fb0bb7d9f8723cdc8
Nalin Dahyabhai <nalin@redhat.com>
2017-02-02 15:24:02 -0500

    Refine the libdm_no_deferred_remove tag check
    Refine the test for the "libdm_no_deferred_remove" build tag so that we
    actually check if we can link with the deferred removal function.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- libdm_tag.sh
+++ libdm_tag.sh
@@ -2,10 +2,11 @@
 tmpdir="$PWD/tmp.$RANDOM"
 mkdir -p "$tmpdir"
 trap 'rm -fr "$tmpdir"' EXIT
-cc -c -o "$tmpdir"/libdm_tag.o -x c - > /dev/null 2> /dev/null << EOF
+cc -o "$tmpdir"/libdm_tag -ldevmapper -x c - > /dev/null 2> /dev/null << EOF
 #include <libdevmapper.h>
 int main() {
 	struct dm_task *task;
+	dm_task_deferred_remove(task);
 	return 0;
 }
 EOF
