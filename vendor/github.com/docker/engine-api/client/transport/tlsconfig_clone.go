6efaa3d56ea2de0c7c9edd134c14671b2a48ad36
Nalin Dahyabhai <nalin@redhat.com>
2017-02-06 14:58:10 -0500

    Another stab at getting go1.6 builds working
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- vendor/github.com/docker/engine-api/client/transport/tlsconfig_clone.go
+++ vendor/github.com/docker/engine-api/client/transport/tlsconfig_clone.go
@@ -1,11 +1,31 @@
-// +build go1.6,!go1.7
+// +build go1.5,!go1.7
 
 package transport
 
 import "crypto/tls"
 
 // TLSConfigClone returns a clone of tls.Config. This function is provided for
-// compatibility for go1.7 that doesn't include this method in stdlib.
+// compatibility for go1.5 and go1.6 that don't include this method in stdlib.
 func TLSConfigClone(c *tls.Config) *tls.Config {
-	return c.Clone()
+	return &tls.Config{
+		Rand:                     c.Rand,
+		Time:                     c.Time,
+		Certificates:             c.Certificates,
+		NameToCertificate:        c.NameToCertificate,
+		GetCertificate:           c.GetCertificate,
+		RootCAs:                  c.RootCAs,
+		NextProtos:               c.NextProtos,
+		ServerName:               c.ServerName,
+		ClientAuth:               c.ClientAuth,
+		ClientCAs:                c.ClientCAs,
+		InsecureSkipVerify:       c.InsecureSkipVerify,
+		CipherSuites:             c.CipherSuites,
+		PreferServerCipherSuites: c.PreferServerCipherSuites,
+		SessionTicketsDisabled:   c.SessionTicketsDisabled,
+		SessionTicketKey:         c.SessionTicketKey,
+		ClientSessionCache:       c.ClientSessionCache,
+		MinVersion:               c.MinVersion,
+		MaxVersion:               c.MaxVersion,
+		CurvePreferences:         c.CurvePreferences,
+	}
 }
