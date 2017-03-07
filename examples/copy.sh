cd27adf0b4b226e7be2a4228490db2568ea30717
Nalin Dahyabhai <nalin@redhat.com>
2017-03-06 20:32:55 -0500

    Add another example script
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

---
+++ examples/copy.sh
@@ -0,0 +1,106 @@
+#!/bin/bash -x
+set -e
+read
+export PATH=`pwd`:$PATH
+d=`mktemp -d`
+trap 'cd /;rm -fr "$d"' EXIT
+cd "$d"
+systemctl restart ocid
+read
+: "[1m Check if we have some images to work with.[0m"
+ocic image list
+read
+: "[1m Create a working container, and capture its name [0m"
+read
+echo '[container1=`buildah from --pull --image ${1:-alpine}`]'
+container1=`buildah from --pull --image ${1:-alpine}`
+read
+: "[1m Mount that working container, and capture the mountpoint [0m"
+read
+echo '[mountpoint1=`buildah mount --name $container1`]'
+mountpoint1=`buildah mount --name $container1`
+read
+: "[1m List some of the container's contents [0m"
+read
+echo '[find $mountpoint1 -name "random*"]'
+find $mountpoint1 -name "random*"
+read
+: "[1m Add a file to the container [0m"
+read
+echo '[dd if=/dev/urandom of=random1 bs=512 count=1]'
+dd if=/dev/urandom of=random1 bs=512 count=1
+read
+echo '[buildah copy --name $container1 random1]'
+buildah copy --name $container1 random1
+read
+: "[1m Change the default destination for copying files [0m"
+read
+echo '[buildah config --name $container1 --workingdir /tmp]'
+buildah config --name $container1 --workingdir /tmp
+read
+: "[1m Add another new file to the container [0m"
+read
+echo '[dd if=/dev/urandom of=random2 bs=512 count=1]'
+dd if=/dev/urandom of=random2 bs=512 count=1
+read
+echo '[buildah copy --name $container1 random2]'
+buildah copy --name $container1 random2
+read
+: "[1m Copy a subdirectory with some files [0m"
+read
+echo '[mkdir -p randomsubdir]'
+echo '[dd if=/dev/urandom of=randomsubdir/random3 bs=512 count=1]'
+echo '[dd if=/dev/urandom of=randomsubdir/random4 bs=512 count=1]'
+mkdir -p randomsubdir
+dd if=/dev/urandom of=randomsubdir/random3 bs=512 count=1
+dd if=/dev/urandom of=randomsubdir/random4 bs=512 count=1
+read
+echo '[buildah copy --name $container1 randomsubdir]'
+buildah copy --name $container1 randomsubdir
+read
+: "[1m List some of the container's contents [0m"
+read
+echo '[find $mountpoint1 -name "random*"]'
+find $mountpoint1 -name "random*"
+read
+: "[1m Grab a tarball [0m"
+read
+echo '[wget -c https://releases.pagure.org/tmpwatch/tmpwatch-2.9.17.tar.bz2]'
+wget -c https://releases.pagure.org/tmpwatch/tmpwatch-2.9.17.tar.bz2
+read
+: "[1m Copy the tarball to the container [0m"
+read
+echo '[mkdir -p $mountpoint1/tmpwatch]'
+echo '[buildah config --name $container1 --workingdir /tmpwatch]'
+echo '[buildah copy --name $container1 tmpwatch-2.9.17.tar.bz2]'
+read
+mkdir -p $mountpoint1/tmpwatch
+buildah config --name $container1 --workingdir /tmpwatch
+buildah copy --name $container1 tmpwatch-2.9.17.tar.bz2
+read
+: "[1m Copy another tarball to the container [0m"
+read
+echo '[buildah copy --name $container1 https://releases.pagure.org/newt/newt-0.52.19.tar.gz]'
+read
+buildah copy --name $container1 https://releases.pagure.org/newt/newt-0.52.19.tar.gz
+read
+: "[1m List some of the container's contents [0m"
+read
+echo '[find $mountpoint1/tmpwatch]'
+find $mountpoint1/tmpwatch
+read
+: "[1m Add the tarball to the container [0m"
+read
+echo '[buildah add --name $container1 tmpwatch-2.9.17.tar.bz2]'
+read
+buildah add --name $container1 tmpwatch-2.9.17.tar.bz2
+read
+: "[1m List some of the container's contents [0m"
+read
+echo '[find $mountpoint1/tmpwatch]'
+find $mountpoint1/tmpwatch
+read
+: "[1m Clean up, because I ran this like fifty times while testing [0m"
+read
+echo '[buildah delete --name $container1]'
+buildah delete --name $container1
