b9b433517362a7dff336ff6eb9590c7971150627
Nalin Dahyabhai <nalin@redhat.com>
2017-03-07 10:41:52 -0500

    Update the copy example script
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- examples/copy.sh
+++ examples/copy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 set -e
-read
+: "[1m Build a temporary directory; make sure ocid is running.[0m"
 export PATH=`pwd`:$PATH
 d=`mktemp -d`
 trap 'cd /;rm -fr "$d"' EXIT
@@ -8,6 +8,7 @@ cd "$d"
 systemctl restart ocid
 read
 : "[1m Check if we have some images to work with.[0m"
+read
 ocic image list
 read
 : "[1m Create a working container, and capture its name [0m"
@@ -20,17 +21,21 @@ read
 echo '[mountpoint1=`buildah mount --name $container1`]'
 mountpoint1=`buildah mount --name $container1`
 read
-: "[1m List some of the container's contents [0m"
+: "[1m List random files in the container [0m"
 read
 echo '[find $mountpoint1 -name "random*"]'
 find $mountpoint1 -name "random*"
 read
+: "[1m Ensure the default destination for copying files is / [0m"
+read
+echo '[buildah config --name $container1 --workingdir /]'
+buildah config --name $container1 --workingdir /
+read
 : "[1m Add a file to the container [0m"
 read
 echo '[dd if=/dev/urandom of=random1 bs=512 count=1]'
-dd if=/dev/urandom of=random1 bs=512 count=1
-read
 echo '[buildah copy --name $container1 random1]'
+dd if=/dev/urandom of=random1 bs=512 count=1
 buildah copy --name $container1 random1
 read
 : "[1m Change the default destination for copying files [0m"
@@ -41,21 +46,19 @@ read
 : "[1m Add another new file to the container [0m"
 read
 echo '[dd if=/dev/urandom of=random2 bs=512 count=1]'
-dd if=/dev/urandom of=random2 bs=512 count=1
-read
 echo '[buildah copy --name $container1 random2]'
+dd if=/dev/urandom of=random2 bs=512 count=1
 buildah copy --name $container1 random2
 read
-: "[1m Copy a subdirectory with some files [0m"
+: "[1m Copy a subdirectory with some files in it [0m"
 read
 echo '[mkdir -p randomsubdir]'
 echo '[dd if=/dev/urandom of=randomsubdir/random3 bs=512 count=1]'
 echo '[dd if=/dev/urandom of=randomsubdir/random4 bs=512 count=1]'
+echo '[buildah copy --name $container1 randomsubdir]'
 mkdir -p randomsubdir
 dd if=/dev/urandom of=randomsubdir/random3 bs=512 count=1
 dd if=/dev/urandom of=randomsubdir/random4 bs=512 count=1
-read
-echo '[buildah copy --name $container1 randomsubdir]'
 buildah copy --name $container1 randomsubdir
 read
 : "[1m List some of the container's contents [0m"
@@ -63,39 +66,34 @@ read
 echo '[find $mountpoint1 -name "random*"]'
 find $mountpoint1 -name "random*"
 read
-: "[1m Grab a tarball [0m"
+: "[1m Download a tarball [0m"
 read
 echo '[wget -c https://releases.pagure.org/tmpwatch/tmpwatch-2.9.17.tar.bz2]'
 wget -c https://releases.pagure.org/tmpwatch/tmpwatch-2.9.17.tar.bz2
 read
-: "[1m Copy the tarball to the container [0m"
+: "[1m Copy that tarball to the container [0m"
 read
 echo '[mkdir -p $mountpoint1/tmpwatch]'
-echo '[buildah config --name $container1 --workingdir /tmpwatch]'
-echo '[buildah copy --name $container1 tmpwatch-2.9.17.tar.bz2]'
-read
+echo '[buildah copy --name $container1 --dest /tmpwatch tmpwatch-2.9.17.tar.bz2]'
 mkdir -p $mountpoint1/tmpwatch
-buildah config --name $container1 --workingdir /tmpwatch
-buildah copy --name $container1 tmpwatch-2.9.17.tar.bz2
+buildah copy --name $container1 --dest /tmpwatch tmpwatch-2.9.17.tar.bz2
 read
-: "[1m Copy another tarball to the container [0m"
+: "[1m Download another tarball to the container [0m"
 read
-echo '[buildah copy --name $container1 https://releases.pagure.org/newt/newt-0.52.19.tar.gz]'
+echo '[buildah copy --name $container1 --dest /tmpwatch https://releases.pagure.org/newt/newt-0.52.19.tar.gz]'
+buildah copy --name $container1 --dest /tmpwatch https://releases.pagure.org/newt/newt-0.52.19.tar.gz
 read
-buildah copy --name $container1 https://releases.pagure.org/newt/newt-0.52.19.tar.gz
-read
-: "[1m List some of the container's contents [0m"
+: "[1m List the contents of the target directory [0m"
 read
 echo '[find $mountpoint1/tmpwatch]'
 find $mountpoint1/tmpwatch
 read
-: "[1m Add the tarball to the container [0m"
+: "[1m Now 'add' the downloaded tarball to the container [0m"
 read
-echo '[buildah add --name $container1 tmpwatch-2.9.17.tar.bz2]'
+echo '[buildah add --name $container1 --dest /tmpwatch tmpwatch-2.9.17.tar.bz2]'
+buildah add --name $container1 --dest /tmpwatch tmpwatch-2.9.17.tar.bz2
 read
-buildah add --name $container1 tmpwatch-2.9.17.tar.bz2
-read
-: "[1m List some of the container's contents [0m"
+: "[1m List the contents of the target directory again [0m"
 read
 echo '[find $mountpoint1/tmpwatch]'
 find $mountpoint1/tmpwatch
