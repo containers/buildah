3bd2228516dc44a35b65f2d3f78212c0a49e7bed
Nalin Dahyabhai <nalin@redhat.com>
2017-03-24 13:48:39 -0400

    Clarify a comment
    Clarify that we're referring to a buildah.Builder object here, and it's
    not a capitalization mistake.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- delete.go
+++ delete.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 )
 
-// Delete removes the working container.  The Builder object should not be used
-// after this method is called.
+// Delete removes the working container.  The buildah.Builder object should not
+// be used after this method is called.
 func (b *Builder) Delete() error {
 	if err := b.store.DeleteContainer(b.ContainerID); err != nil {
 		return fmt.Errorf("error deleting build container: %v", err)
