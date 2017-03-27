e5fe19c634bd1f28a061a90d55d31fb460c41c87
Daniel J Walsh <dwalsh@redhat.com>
2017-03-27 08:40:32 -0400

    Switch `buildah list quiet` option to only list container id's
    Add `noheading` option to eliminate headings

--- cmd/buildah/list.go
+++ cmd/buildah/list.go
@@ -12,7 +12,11 @@ var (
 	listFlags = []cli.Flag{
 		cli.BoolFlag{
 			Name:  "quiet, q",
-			Usage: "omit column headings",
+			Usage: "list only container image id's",
+		},
+		cli.BoolFlag{
+			Name:  "noheading, n",
+			Usage: "do not print column headings",
 		},
 	}
 	listDescription = "Lists containers which appear to be " + buildah.Package + " working containers, their\n   names and IDs, and the names and IDs of the images from which they were\n   initialized"
@@ -38,18 +42,27 @@ func listCmd(c *cli.Context) error {
 		quiet = c.Bool("quiet")
 	}
 
+	noheading := false
+	if c.IsSet("noheading") {
+		noheading = c.Bool("noheading")
+	}
+
 	builders, err := openBuilders(store)
 	if err != nil {
 		return fmt.Errorf("error reading build containers: %v", err)
 	}
-	if len(builders) > 0 && !quiet {
+	if len(builders) > 0 && !noheading && !quiet {
 		fmt.Printf("%-64s %-64s %-10s %s\n", "CONTAINER ID", "IMAGE ID", "IMAGE NAME", "CONTAINER NAME")
 	}
 	for _, builder := range builders {
 		if builder.FromImage == "" {
 			builder.FromImage = buildah.BaseImageFakeName
 		}
-		fmt.Printf("%-64s %-64s %-10s %s\n", builder.ContainerID, builder.FromImageID, builder.FromImage, builder.Container)
+		if quiet {
+			fmt.Printf("%-64s\n", builder.ContainerID)
+		} else {
+			fmt.Printf("%-64s %-64s %-10s %s\n", builder.ContainerID, builder.FromImageID, builder.FromImage, builder.Container)
+		}
 	}
 
 	return nil
