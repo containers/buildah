efad757ed413261d250c0d53d2807862eb0c94b3
Daniel J Walsh <dwalsh@redhat.com>
2017-03-28 14:25:40 -0400

    Add truncation to buildah containers|images

--- cmd/buildah/containers.go
+++ cmd/buildah/containers.go
@@ -17,6 +17,10 @@ var (
 			Name:  "noheading, n",
 			Usage: "do not print column headings",
 		},
+		cli.BoolFlag{
+			Name:  "notruncate",
+			Usage: "do not truncate output",
+		},
 	}
 	containersDescription = "Lists containers which appear to be " + buildah.Package + " working containers, their\n   names and IDs, and the names and IDs of the images from which they were\n   initialized"
 	containersCommand     = cli.Command{
@@ -43,13 +47,21 @@ func containersCmd(c *cli.Context) error {
 	if c.IsSet("noheading") {
 		noheading = c.Bool("noheading")
 	}
+	truncate := true
+	if c.IsSet("notruncate") {
+		truncate = !c.Bool("notruncate")
+	}
 
 	builders, err := openBuilders(store)
 	if err != nil {
 		return fmt.Errorf("error reading build containers: %v", err)
 	}
 	if len(builders) > 0 && !noheading && !quiet {
-		fmt.Printf("%-64s %-64s %-10s %s\n", "CONTAINER ID", "IMAGE ID", "IMAGE NAME", "CONTAINER NAME")
+		if truncate {
+			fmt.Printf("%-12s %-12s %-10s %s\n", "CONTAINER ID", "IMAGE ID", "IMAGE NAME", "CONTAINER NAME")
+		} else {
+			fmt.Printf("%-64s %-64s %-10s %s\n", "CONTAINER ID", "IMAGE ID", "IMAGE NAME", "CONTAINER NAME")
+		}
 	}
 	for _, builder := range builders {
 		if builder.FromImage == "" {
@@ -58,7 +70,11 @@ func containersCmd(c *cli.Context) error {
 		if quiet {
 			fmt.Printf("%s\n", builder.ContainerID)
 		} else {
-			fmt.Printf("%-64s %-64s %-10s %s\n", builder.ContainerID, builder.FromImageID, builder.FromImage, builder.Container)
+			if truncate {
+				fmt.Printf("%-12.12s %-12.12s %-10s %s\n", builder.ContainerID, builder.FromImageID, builder.FromImage, builder.Container)
+			} else {
+				fmt.Printf("%-64s %-64s %-10s %s\n", builder.ContainerID, builder.FromImageID, builder.FromImage, builder.Container)
+			}
 		}
 	}
 
