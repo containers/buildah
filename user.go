b1bb73e01c9bf0b1b75e50a2d1947b14a8174eee
Nalin Dahyabhai <nalin@redhat.com>
2017-04-04 17:31:02 -0400

    Teach "Run" to dig user IDs out of containers
    When we have cgo, use fgetpwent() to try to look up user IDs and primary
    GIDs in containers.  If that fails for any reason (or if we don't have
    cgo), fall back to doing what we were doing before (i.e., trying to look
    up the information on the host).
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>
    
    Closes: #63
    Approved by: rhatdan

--- user.go
+++ user.go
@@ -1,33 +1,78 @@
 package buildah
 
 import (
+	"fmt"
 	"os/user"
 	"strconv"
+	"strings"
 
 	"github.com/opencontainers/runtime-spec/specs-go"
 )
 
-// TODO: we should doing these lookups using data that's actually in the container.
-func getUser(username string) (specs.User, error) {
-	if username == "" {
+func getUser(rootdir, userspec string) (specs.User, error) {
+	var gid64 uint64
+	var gerr error = user.UnknownGroupError("error looking up group")
+
+	spec := strings.SplitN(userspec, ":", 2)
+	userspec = spec[0]
+	groupspec := ""
+	if userspec == "" {
 		return specs.User{}, nil
 	}
-	runuser, err := user.Lookup(username)
-	if err != nil {
-		return specs.User{}, err
+	if len(spec) > 1 {
+		groupspec = spec[1]
 	}
-	uid, err := strconv.ParseUint(runuser.Uid, 10, 32)
-	if err != nil {
-		return specs.User{}, nil
+
+	uid64, uerr := strconv.ParseUint(userspec, 10, 32)
+	if uerr == nil && groupspec == "" {
+		// We parsed the user name as a number, and there's no group
+		// component, so we need to look up the user's primary GID.
+		name := ""
+		name, gid64, gerr = lookupGroupForUIDInContainer(rootdir, uid64)
+		if gerr == nil {
+			userspec = name
+		} else {
+			if userrec, err := user.LookupId(userspec); err == nil {
+				gid64, gerr = strconv.ParseUint(userrec.Gid, 10, 32)
+				userspec = userrec.Name
+			}
+		}
 	}
-	gid, err := strconv.ParseUint(runuser.Gid, 10, 32)
-	if err != nil {
-		return specs.User{}, nil
+	if uerr != nil {
+		uid64, gid64, uerr = lookupUserInContainer(rootdir, userspec)
+		gerr = uerr
 	}
-	u := specs.User{
-		UID:      uint32(uid),
-		GID:      uint32(gid),
-		Username: username,
+	if uerr != nil {
+		if userrec, err := user.Lookup(userspec); err == nil {
+			uid64, uerr = strconv.ParseUint(userrec.Uid, 10, 32)
+			gid64, gerr = strconv.ParseUint(userrec.Gid, 10, 32)
+		}
 	}
-	return u, nil
+
+	if groupspec != "" {
+		gid64, gerr = strconv.ParseUint(groupspec, 10, 32)
+		if gerr != nil {
+			gid64, gerr = lookupGroupInContainer(rootdir, groupspec)
+		}
+		if gerr != nil {
+			if group, err := user.LookupGroup(groupspec); err == nil {
+				gid64, gerr = strconv.ParseUint(group.Gid, 10, 32)
+			}
+		}
+	}
+
+	if uerr == nil && gerr == nil {
+		u := specs.User{
+			UID:      uint32(uid64),
+			GID:      uint32(gid64),
+			Username: userspec,
+		}
+		return u, nil
+	}
+
+	err := fmt.Errorf("%v determining run uid", uerr)
+	if uerr == nil {
+		err = fmt.Errorf("%v determining run gid", gerr)
+	}
+	return specs.User{}, err
 }
