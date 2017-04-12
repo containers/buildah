1f27da41f7699ac7ab1893c92e4cb0034b91e3ea
Nalin Dahyabhai <nalin@redhat.com>
2017-04-12 13:31:34 -0400

    Never use host methods for parsing USER values
    Drop fallbacks for resolving USER values that attempt to look up names
    on the host, since that's never predictable.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- user.go
+++ user.go
@@ -31,33 +31,17 @@ func getUser(rootdir, userspec string) (specs.User, error) {
 		name, gid64, gerr = lookupGroupForUIDInContainer(rootdir, uid64)
 		if gerr == nil {
 			userspec = name
-		} else {
-			if userrec, err := user.LookupId(userspec); err == nil {
-				gid64, gerr = strconv.ParseUint(userrec.Gid, 10, 32)
-				userspec = userrec.Name
-			}
 		}
 	}
 	if uerr != nil {
 		uid64, gid64, uerr = lookupUserInContainer(rootdir, userspec)
 		gerr = uerr
 	}
-	if uerr != nil {
-		if userrec, err := user.Lookup(userspec); err == nil {
-			uid64, uerr = strconv.ParseUint(userrec.Uid, 10, 32)
-			gid64, gerr = strconv.ParseUint(userrec.Gid, 10, 32)
-		}
-	}
 
 	if groupspec != "" {
 		gid64, gerr = strconv.ParseUint(groupspec, 10, 32)
 		if gerr != nil {
 			gid64, gerr = lookupGroupInContainer(rootdir, groupspec)
-		}
-		if gerr != nil {
-			if group, err := user.LookupGroup(groupspec); err == nil {
-				gid64, gerr = strconv.ParseUint(group.Gid, 10, 32)
-			}
 		}
 	}
 
