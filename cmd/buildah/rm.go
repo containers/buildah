82a34a1ca26b3100f4b21c403975af02d0cf3655
Nalin Dahyabhai <nalin@redhat.com>
2017-04-13 16:28:56 -0400

    rm/rmi: Don't exit until we've tried them all
    If we're told to remove more than one container or image, actually try
    to remove them all, but still return an error if we fail at any of them.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- cmd/buildah/rm.go
+++ cmd/buildah/rm.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/urfave/cli"
 )
@@ -28,17 +29,28 @@ func rmCmd(c *cli.Context) error {
 		return err
 	}
 
+	var e error
 	for _, name := range args {
 		builder, err := openBuilder(store, name)
+		if e == nil {
+			e = err
+		}
 		if err != nil {
-			return fmt.Errorf("error reading build container %q: %v", name, err)
+			fmt.Fprintf(os.Stderr, "error reading build container %q: %v\n", name, err)
+			continue
 		}
 
+		id := builder.ContainerID
 		err = builder.Delete()
-		if err != nil {
-			return fmt.Errorf("error removing container %q: %v", builder.Container, err)
+		if e == nil {
+			e = err
 		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error removing container %q: %v\n", builder.Container, err)
+			continue
+		}
+		fmt.Printf("%s\n", id)
 	}
 
-	return nil
+	return e
 }
