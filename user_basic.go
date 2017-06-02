972373d89e98a9972b06184520c7dd96415b1da0
Nalin Dahyabhai <nalin@redhat.com>
2017-06-02 10:39:26 -0400

    Use errors.New() instead of Wrapf() for new errors
    Call errors.New() instead of calling errors.Wrapf() when there's no
    error to wrap.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- user_basic.go
+++ user_basic.go
@@ -3,17 +3,17 @@
 package buildah
 
 import (
-	"fmt"
+	"github.com/pkg/errors"
 )
 
 func lookupUserInContainer(rootdir, username string) (uint64, uint64, error) {
-	return 0, 0, errors.Wrapf("user lookup not supported")
+	return 0, 0, errors.New("user lookup not supported")
 }
 
 func lookupGroupInContainer(rootdir, groupname string) (uint64, error) {
-	return 0, errors.Wrapf("group lookup not supported")
+	return 0, errors.New("group lookup not supported")
 }
 
 func lookupGroupForUIDInContainer(rootdir string, userid uint64) (string, uint64, error) {
-	return "", 0, errors.Wrapf("primary group lookup by uid not supported")
+	return "", 0, errors.New("primary group lookup by uid not supported")
 }
