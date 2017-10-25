ef54759ad36d8c50d20cdfb95a2c623f261f38e9
Nalin Dahyabhai <nalin@redhat.com>
2017-10-24 23:02:13 -0400

    Bump containers/image and containers/storage
    Update containers/image and containers/storage to current master
    (17449738f2bb4c6375c20dcdcfe2a6cccf03f312 and
    0d32dfce498e06c132c60dac945081bf44c22464, respectively).
    
    Also updates github.com/docker/docker, golang.org/x/sys, and
    golang.org/x/text.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- vendor/github.com/containers/image/directory/directory_transport.go
+++ vendor/github.com/containers/image/directory/directory_transport.go
@@ -134,13 +134,14 @@ func (ref dirReference) PolicyConfigurationNamespaces() []string {
 	return res
 }
 
-// NewImage returns a types.Image for this reference, possibly specialized for this ImageTransport.
-// The caller must call .Close() on the returned Image.
+// NewImage returns a types.ImageCloser for this reference, possibly specialized for this ImageTransport.
+// The caller must call .Close() on the returned ImageCloser.
 // NOTE: If any kind of signature verification should happen, build an UnparsedImage from the value returned by NewImageSource,
 // verify that UnparsedImage, and convert it into a real Image via image.FromUnparsedImage.
-func (ref dirReference) NewImage(ctx *types.SystemContext) (types.Image, error) {
+// WARNING: This may not do the right thing for a manifest list, see image.FromSource for details.
+func (ref dirReference) NewImage(ctx *types.SystemContext) (types.ImageCloser, error) {
 	src := newImageSource(ref)
-	return image.FromSource(src)
+	return image.FromSource(ctx, src)
 }
 
 // NewImageSource returns a types.ImageSource for this reference.
