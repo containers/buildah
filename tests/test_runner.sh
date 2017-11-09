318beaa7205b58e8e61803b0f0d2b3bf21462ca5
Nalin Dahyabhai <nalin@redhat.com>
2017-11-09 11:26:15 -0500

    integration tests: default to /var/tmp
    Default to running integration tests using /var/tmp as scratch space,
    since it's more likely to support proper SELinux labeling than /tmp,
    which is more likely to be on a tmpfs.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>
    
    Closes: #320
    Approved by: nalind

--- tests/test_runner.sh
+++ tests/test_runner.sh
@@ -3,6 +3,10 @@ set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
+# Default to using /var/tmp for test space, since it's more likely to support
+# labels than /tmp, which is often on tmpfs.
+export TMPDIR=${TMPDIR:-/var/tmp}
+
 # Load the helpers.
 . helpers.bash
 
