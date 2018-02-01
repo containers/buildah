bc9d574c1071052393463787049c879ed394ca2d
umohnani8 <umohnani@redhat.com>
2018-02-01 12:46:23 -0500

    Add new line after executing template for buildah inspect
    No new line was returned when using the --format flag for buildah
    inspect.
    
    Signed-off-by: umohnani8 <umohnani@redhat.com>
    
    Closes: #442
    Approved by: rhatdan

--- cmd/buildah/images.go
+++ cmd/buildah/images.go
@@ -14,6 +14,7 @@ import (
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 type jsonImage struct {
@@ -372,7 +373,9 @@ func outputUsingTemplate(format string, params imageOutputParams) error {
 	if err != nil {
 		return err
 	}
-	fmt.Println()
+	if terminal.IsTerminal(int(os.Stdout.Fd())) {
+		fmt.Println()
+	}
 	return nil
 }
 
