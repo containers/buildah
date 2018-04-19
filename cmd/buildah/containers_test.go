28a27a3c48bb58afc87718f42237bc711240c63d
Zhou Hao <zhouhao@cn.fujitsu.com>
2018-04-19 14:13:23 +0800

    containers_test: Perfect testing
    1.Rename some function names and distinguish them from test functions in
    images_test.
    2.Improve test content in containers_test.
    
    Signed-off-by: Zhou Hao <zhouhao@cn.fujitsu.com>
    
    Closes: #604
    Approved by: rhatdan

--- cmd/buildah/containers_test.go
+++ cmd/buildah/containers_test.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 )
 
-func TestTemplateOutputValidFormat(t *testing.T) {
+func TestContainerTemplateOutputValidFormat(t *testing.T) {
 	params := containerOutputParams{
 		ContainerID:   "e477836657bb",
 		Builder:       " ",
@@ -27,7 +28,7 @@ func TestTemplateOutputValidFormat(t *testing.T) {
 	}
 }
 
-func TestTemplateOutputInvalidFormat(t *testing.T) {
+func TestContainerTemplateOutputInvalidFormat(t *testing.T) {
 	params := containerOutputParams{
 		ContainerID:   "e477836657bb",
 		Builder:       " ",
@@ -41,5 +42,66 @@ func TestTemplateOutputInvalidFormat(t *testing.T) {
 	err := containerOutputUsingTemplate(formatString, params)
 	if err == nil || err.Error() != "error invalid format provided: ContainerID" {
 		t.Fatalf("expected error invalid format")
+	}
+}
+
+func TestContainerTemplateOutputInexistenceField(t *testing.T) {
+	params := containerOutputParams{
+		ContainerID:   "e477836657bb",
+		Builder:       " ",
+		ImageID:       "f975c5035748",
+		ImageName:     "test/image:latest",
+		ContainerName: "test-container",
+	}
+
+	formatString := "{{.ID}}"
+
+	err := containerOutputUsingTemplate(formatString, params)
+	if err == nil || !strings.Contains(err.Error(), "can't evaluate field ID") {
+		t.Fatalf("expected error inexistence field")
+	}
+}
+
+func TestContainerFormatStringOutput(t *testing.T) {
+	params := containerOutputParams{
+		ContainerID:   "e477836657bb",
+		Builder:       " ",
+		ImageID:       "f975c5035748",
+		ImageName:     "test/image:latest",
+		ContainerName: "test-container",
+	}
+
+	output := captureOutput(func() {
+		containerOutputUsingFormatString(true, params)
+	})
+	expectedOutput := fmt.Sprintf("%-12.12s  %-8s %-12.12s %-32s %s\n", params.ContainerID, params.Builder, params.ImageID, params.ImageName, params.ContainerName)
+	if output != expectedOutput {
+		t.Errorf("Error outputting using format string:\n\texpected: %s\n\treceived: %s\n", expectedOutput, output)
+	}
+
+	output = captureOutput(func() {
+		containerOutputUsingFormatString(false, params)
+	})
+	expectedOutput = fmt.Sprintf("%-64s %-8s %-64s %-32s %s\n", params.ContainerID, params.Builder, params.ImageID, params.ImageName, params.ContainerName)
+	if output != expectedOutput {
+		t.Errorf("Error outputting using format string:\n\texpected: %s\n\treceived: %s\n", expectedOutput, output)
+	}
+}
+
+func TestContainerHeaderOutput(t *testing.T) {
+	output := captureOutput(func() {
+		containerOutputHeader(true)
+	})
+	expectedOutput := fmt.Sprintf("%-12s  %-8s %-12s %-32s %s\n", "CONTAINER ID", "BUILDER", "IMAGE ID", "IMAGE NAME", "CONTAINER NAME")
+	if output != expectedOutput {
+		t.Errorf("Error outputting using format string:\n\texpected: %s\n\treceived: %s\n", expectedOutput, output)
+	}
+
+	output = captureOutput(func() {
+		containerOutputHeader(false)
+	})
+	expectedOutput = fmt.Sprintf("%-64s %-8s %-64s %-32s %s\n", "CONTAINER ID", "BUILDER", "IMAGE ID", "IMAGE NAME", "CONTAINER NAME")
+	if output != expectedOutput {
+		t.Errorf("Error outputting using format string:\n\texpected: %s\n\treceived: %s\n", expectedOutput, output)
 	}
 }
