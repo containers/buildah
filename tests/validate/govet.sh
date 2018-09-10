bbbf6b20908c6020173bea9016007a2bd2680724
Nalin Dahyabhai <nalin@redhat.com>
2018-09-10 14:23:26 -0400

    Don't build btrfs if it is not installed
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>
    
    Closes: #1016
    Approved by: TomSweeneyRedHat

--- tests/validate/govet.sh
+++ tests/validate/govet.sh
@@ -6,7 +6,7 @@ if ! test -x ./btrfs_tag.sh ; then
 	echo govet.sh unable to locate top-level directory, failing.
 	exit 1
 fi
-tags="$(./btrfs_tag.sh) $(./libdm_tag.sh) $(./ostree_tag.sh) $(./selinux_tag.sh)"
+tags="$(./btrfs_tag.sh) $(./btrfs_installed_tag.sh) $(./libdm_tag.sh) $(./ostree_tag.sh) $(./selinux_tag.sh)"
 
 for package in $(go list ./... | grep -v /vendor/) ; do
 	if ! go vet -tags "$tags" ${package} ; then
