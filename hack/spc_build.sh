8df3a657c78c00964ded4c039cf126359ad0f416
Lokesh Mandvekar <lsm5@fedoraproject.org>
2018-09-26 15:17:05 -0400

    tests: run conformance test suite in Travis
    Run conformance tests in after_script section of Travis with
    non-blocking failures.
    
    Follow up to #1003
    
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

--- hack/spc_build.sh
+++ hack/spc_build.sh
@@ -21,11 +21,10 @@ dnf -y update --best --allowerasing
 dnf -y install dnf-plugins-core
 dnf -y copr enable lsm5/container-diff
 dnf -y install autoconf automake btrfs-progs-devel \
-   buildah \
    container-diff device-mapper-devel golang go-md2man \
    git glibc-static gpgme-devel hostname iproute \
    iputils libassuan-devel libseccomp-static make \
-   moby-engine openssl
+   moby-engine podman
 
 # build buildah binary in fedora and run tests
 echo "Cleanup buildah repo and build again in fedora..."
@@ -37,7 +36,7 @@ make GOPATH=$(pwd)/_build:$(pwd) all TAGS="seccomp containers_image_ostree_stub"
 GOPATH=$(pwd)/_build:$(pwd) go test -c -tags "seccomp `./btrfs_tag.sh` `./btrfs_installed_tag.sh` `./libdm_tag.sh` `./ostree_tag.sh` `./selinux_tag.sh`" ./cmd/buildah
 tmp=$(mktemp -d); mkdir $tmp/root $tmp/runroot; PATH="$PATH" ./buildah.test -test.v -root $tmp/root -runroot $tmp/runroot -storage-driver vfs -signature-policy $(pwd)/tests/policy.json -registries-conf $(pwd)/tests/registries.conf
 
-echo "docker login to local registry..."
+echo "Logging in to local registry..."
 echo testpassword | docker login localhost:5000 --username testuser --password-stdin
 
 echo "docker build dockerfile..."
@@ -58,3 +57,7 @@ docker pull localhost:5000/buildah-test-image
 echo "Running container-diff..."
 container-diff diff --type=rpm daemon://localhost:5000/buildah-test-image daemon://docker-test-image
 
+echo "Running conformance tests..."
+GOPATH=$(pwd) go test -v ./tests/conformance
+
+echo "Finished conformance tests. Exiting..."
