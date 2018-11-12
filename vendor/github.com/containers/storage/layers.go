33834738fd523c4fc9fa22474201672d17e0a333
Giuseppe Scrivano <gscrivan@redhat.com>
2018-11-12 11:56:01 +0100

    vendor: update containers/storage
    inherit some fixes for rootless overlay with fuse-overlayfs.
    
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- vendor/github.com/containers/storage/layers.go
+++ vendor/github.com/containers/storage/layers.go
@@ -542,8 +542,8 @@ func (r *layerStore) Put(id string, parentLayer *Layer, names []string, mountLab
 			_, idInUse = r.byid[id]
 		}
 	}
-	if _, idInUse := r.byid[id]; idInUse {
-		return nil, -1, ErrDuplicateID
+	if duplicateLayer, idInUse := r.byid[id]; idInUse {
+		return duplicateLayer, -1, ErrDuplicateID
 	}
 	names = dedupeNames(names)
 	for _, name := range names {
