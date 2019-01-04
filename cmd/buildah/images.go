fb4b864d63a15cc531e9cf2b14ce4c79ac70eb3b
Tristan Cacqueray <tdecacqu@redhat.com>
2019-01-04 05:23:38 +0000

    Properly format images JSON output
    This change makes the images --json output a valid JSON list.
    
    Closes: #1257
    Signed-off-by: Tristan Cacqueray <tdecacqu@redhat.com>
    
    Closes: #1258
    Approved by: TomSweeneyRedHat

--- cmd/buildah/images.go
+++ cmd/buildah/images.go
@@ -243,6 +243,7 @@ func outputHeader(truncate, digests bool) {
 
 func outputImages(ctx context.Context, images []storage.Image, store storage.Store, filters *filterParams, argName string, opts imageOptions) error {
 	found := false
+	jsonImages := []jsonImage{}
 	for _, image := range images {
 		createdTime := image.Created
 
@@ -291,12 +292,7 @@ func outputImages(ctx context.Context, images []storage.Image, store storage.Sto
 					break outer
 				}
 				if opts.json {
-					JSONImage := jsonImage{ID: image.ID, Names: image.Names}
-					data, err2 := json.MarshalIndent(JSONImage, "", "    ")
-					if err2 != nil {
-						return err2
-					}
-					fmt.Printf("%s\n", data)
+					jsonImages = append(jsonImages, jsonImage{ID: image.ID, Names: image.Names})
 					// We only want to print each id once
 					break outer
 				}
@@ -321,6 +317,13 @@ func outputImages(ctx context.Context, images []storage.Image, store storage.Sto
 
 	if !found && argName != "" {
 		return errors.Errorf("No such image %s", argName)
+	}
+	if opts.json {
+		data, err := json.MarshalIndent(jsonImages, "", "    ")
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%s\n", data)
 	}
 
 	return nil
