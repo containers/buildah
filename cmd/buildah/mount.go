containers/buildah

commit 3d7403130100c3558ea14a1c21482559109de043

Move pkg/chrootuser from libpod to buildah.
We don't want to vendor anything from libpod into Buildah.
We want to switch this around.  Moving pkg content from libpod
to Buildah allows us to fix this.

Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

Closes: #1400
Approved by: giuseppe

cmd/buildah/mount.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	buildahcli "github.com/containers/buildah/pkg/cli"
+	"github.com/containers/buildah/unshare"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
@@ -62,7 +63,7 @@ func mountCmd(c *cobra.Command, args []string, noTruncate bool) error {
 		// of the mount command.
 		// Differently, allow the mount if we are already in a userns, as the mount point will still
 		// be accessible once "buildah mount" exits.
-		if os.Getenv(startedInUserNS) != "" && store.GraphDriverName() != "vfs" {
+		if unshare.IsRootless() && store.GraphDriverName() != "vfs" {
 			return fmt.Errorf("cannot mount using driver %s in rootless mode. You need to run it in a `buildah unshare` session", store.GraphDriverName())
 		}
 

cmd/buildah/unshare.go
@@ -20,12 +20,6 @@ import (
 	"github.com/syndtr/gocapability/capability"
 )
 
-const (
-	// startedInUserNS is an environment variable that, if set, means that we shouldn't try
-	// to create and enter a new user namespace and then re-exec ourselves.
-	startedInUserNS = "_BUILDAH_STARTED_IN_USERNS"
-)
-
 var (
 	unshareDescription = "\n  Runs a command in a modified user namespace."
 	unshareCommand     = &cobra.Command{
@@ -63,7 +57,7 @@ func bailOnError(err error, format string, a ...interface{}) {
 
 func maybeReexecUsingUserNamespace(cmdName string, evenForRoot bool) {
 	// If we've already been through this once, no need to try again.
-	if os.Getenv(startedInUserNS) != "" {
+	if unshare.IsRootless() {
 		return
 	}
 
@@ -141,8 +135,8 @@ func maybeReexecUsingUserNamespace(cmdName string, evenForRoot bool) {
 	cmd := unshare.Command(append([]string{"buildah-in-a-user-namespace"}, os.Args[1:]...)...)
 
 	// If, somehow, we don't become UID 0 in our child, indicate that the child shouldn't try again.
-	err = os.Setenv(startedInUserNS, "1")
-	bailOnError(err, "error setting %s=1 in environment", startedInUserNS)
+	err = os.Setenv(unshare.UsernsEnvName, "1")
+	bailOnError(err, "error setting %s=1 in environment", unshare.UsernsEnvName)
 
 	// Set the default isolation type to use the "rootless" method.
 	if _, present := os.LookupEnv("BUILDAH_ISOLATION"); !present {
@@ -210,7 +204,7 @@ func unshareCmd(c *cobra.Command, args []string) error {
 		args = []string{shell}
 	}
 	cmd := exec.Command(args[0], args[1:]...)
-	cmd.Env = append(os.Environ(), "_BUILDAH_STARTED_IN_USERNS=")
+	cmd.Env = unshare.RootlessEnv()
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr

unshare/unshare.go
@@ -11,6 +11,7 @@ import (
 	"runtime"
 	"strconv"
 	"strings"
+	"sync"
 	"syscall"
 
 	"github.com/containers/buildah/util"
@@ -57,8 +58,8 @@ func (c *Cmd) Start() error {
 
 	// Please the libpod "rootless" package to find the expected env variables.
 	if os.Geteuid() != 0 {
-		c.Env = append(c.Env, "_LIBPOD_USERNS_CONFIGURED=done")
-		c.Env = append(c.Env, fmt.Sprintf("_LIBPOD_ROOTLESS_UID=%d", os.Geteuid()))
+		c.Env = append(c.Env, "_CONTAINERS_USERNS_CONFIGURED=done")
+		c.Env = append(c.Env, fmt.Sprintf("_CONTAINERS_ROOTLESS_UID=%d", os.Geteuid()))
 	}
 
 	// Create the pipe for reading the child's PID.
@@ -271,4 +272,37 @@ func (c *Cmd) CombinedOutput() ([]byte, error) {
 
 func (c *Cmd) Output() ([]byte, error) {
 	return nil, errors.New("unshare: Output() not implemented")
+}
+
+var (
+	isRootlessOnce sync.Once
+	isRootless     bool
+)
+
+const (
+	// UsernsEnvName is the environment variable, if set indicates in rootless mode
+	UsernsEnvName = "_CONTAINERS_USERNS_CONFIGURED"
+)
+
+// IsRootless tells us if we are running in rootless mode
+func IsRootless() bool {
+	isRootlessOnce.Do(func() {
+		isRootless = os.Geteuid() != 0 || os.Getenv(UsernsEnvName) != ""
+	})
+	return isRootless
+}
+
+// GetRootlessUID returns the UID of the user in the parent userNS
+func GetRootlessUID() int {
+	uidEnv := os.Getenv("_CONTAINERS_ROOTLESS_UID")
+	if uidEnv != "" {
+		u, _ := strconv.Atoi(uidEnv)
+		return u
+	}
+	return os.Getuid()
+}
+
+// RootlessEnv returns the environment settings for the rootless containers
+func RootlessEnv() []string {
+	return append(os.Environ(), UsernsEnvName+"=")
 }

commit 727f70e31e95a9d53cb52003eaa67742107bca51

buildah: correctly create the userns if euid!=0
we were skipping the creation of the creation of a namespace if the
euid != 0.

regression introduced by 3d7403130100c3558ea14a1c21482559109de043

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

cmd/buildah/mount.go
@@ -5,7 +5,6 @@ import (
 	"os"
 
 	buildahcli "github.com/containers/buildah/pkg/cli"
-	"github.com/containers/buildah/unshare"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
@@ -63,7 +62,7 @@ func mountCmd(c *cobra.Command, args []string, noTruncate bool) error {
 		// of the mount command.
 		// Differently, allow the mount if we are already in a userns, as the mount point will still
 		// be accessible once "buildah mount" exits.
-		if unshare.IsRootless() && store.GraphDriverName() != "vfs" {
+		if os.Geteuid() != 0 && store.GraphDriverName() != "vfs" {
 			return fmt.Errorf("cannot mount using driver %s in rootless mode. You need to run it in a `buildah unshare` session", store.GraphDriverName())
 		}
 

cmd/buildah/unshare.go
@@ -57,7 +57,7 @@ func bailOnError(err error, format string, a ...interface{}) {
 
 func maybeReexecUsingUserNamespace(cmdName string, evenForRoot bool) {
 	// If we've already been through this once, no need to try again.
-	if unshare.IsRootless() {
+	if os.Geteuid() == 0 && unshare.IsRootless() {
 		return
 	}
 

unshare/unshare.go
@@ -304,5 +304,5 @@ func GetRootlessUID() int {
 
 // RootlessEnv returns the environment settings for the rootless containers
 func RootlessEnv() []string {
-	return append(os.Environ(), UsernsEnvName+"=")
+	return append(os.Environ(), UsernsEnvName+"=done")
 }