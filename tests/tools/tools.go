e160a632b7f571cd4c20d0e1240e47d02f82e9b9
Sascha Grunert <sgrunert@suse.com>
2019-07-04 14:36:46 +0200

    Switch to golangci-lint
    This commit vendors golangci-lint and enables all linters which do not
    complain right now. CI and scripting has been adapted as well.
    
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>
    
    Closes: #1706
    Approved by: rhatdan

--- tests/tools/tools.go
+++ tests/tools/tools.go
@@ -7,6 +7,7 @@ package tools
 
 import (
 	_ "github.com/cpuguy83/go-md2man"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/onsi/ginkgo/ginkgo"
 	_ "github.com/vbatts/git-validation"
 )
