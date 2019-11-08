92e59d524966f62922589936ec894ad2117b14d1
Chris Evich <cevich@redhat.com>
2019-11-08 10:10:23 -0500

    Show validation command-line
    Validation problems can be difficult to debug w/o knowing what options
    were passed / how it was called.
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- tests/validate/git-validation.sh
+++ tests/validate/git-validation.sh
@@ -9,4 +9,5 @@ if test "$TRAVIS" != true ; then
 	#GITVALIDATE_EPOCH=":/git-validation epoch"
 	GITVALIDATE_EPOCH="8b1d11f52721ab062c587abc711b40a6e1a1f7dc"
 fi
+set -x
 exec git-validation -q -run DCO,short-subject ${GITVALIDATE_EPOCH:+-range "${GITVALIDATE_EPOCH}""..${GITVALIDATE_TIP:-@}"} ${GITVALIDATE_FLAGS}
