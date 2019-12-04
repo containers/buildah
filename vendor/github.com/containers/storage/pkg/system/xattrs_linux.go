2a82d0737c846743b35ef6933ab683c4b1f18f7f
Sascha Grunert <sgrunert@suse.com>
2019-12-04 11:05:22 +0100

    Vendor containers/storage fix
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>
    
    Closes: #1998
    Approved by: rhatdan

--- vendor/github.com/containers/storage/pkg/system/xattrs_linux.go
+++ vendor/github.com/containers/storage/pkg/system/xattrs_linux.go
@@ -2,7 +2,6 @@ package system
 
 import (
 	"bytes"
-	"math"
 	"syscall"
 
 	"golang.org/x/sys/unix"
@@ -20,22 +19,28 @@ const (
 // and associated with the given path in the file system.
 // It will returns a nil slice and nil error if the xattr is not set.
 func Lgetxattr(path string, attr string) ([]byte, error) {
+	// Start with a 128 length byte array
 	dest := make([]byte, 128)
 	sz, errno := unix.Lgetxattr(path, attr, dest)
-	if errno == unix.ENODATA {
+
+	switch {
+	case errno == unix.ENODATA:
 		return nil, nil
-	}
-	if errno == unix.ERANGE {
-		if sz > math.MaxUint16 {
-			return nil, unix.E2BIG
+	case errno == unix.ERANGE:
+		// 128 byte array might just not be good enough. A dummy buffer is used
+		// to get the real size of the xattrs on disk
+		sz, errno = unix.Lgetxattr(path, attr, []byte{})
+		if errno != nil {
+			return nil, errno
 		}
 		dest = make([]byte, sz)
 		sz, errno = unix.Lgetxattr(path, attr, dest)
-	}
-	if errno != nil {
+		if errno != nil {
+			return nil, errno
+		}
+	case errno != nil:
 		return nil, errno
 	}
-
 	return dest[:sz], nil
 }
 
