e224c09ca0f40537547cfb8591bc1526870e02e2
Sascha Grunert <sgrunert@suse.com>
2020-01-21 15:53:29 +0100

    Fix build for 32bit platforms
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

--- util/util_linux.go
+++ util/util_linux.go
@@ -2,17 +2,18 @@ package util
 
 import (
 	"syscall"
+
+	"golang.org/x/sys/unix"
 )
 
 // IsCgroup2UnifiedMode returns whether we are running in cgroup 2 cgroup2 mode.
 func IsCgroup2UnifiedMode() (bool, error) {
 	isUnifiedOnce.Do(func() {
-		_cgroup2SuperMagic := int64(0x63677270)
 		var st syscall.Statfs_t
 		if err := syscall.Statfs("/sys/fs/cgroup", &st); err != nil {
 			isUnified, isUnifiedErr = false, err
 		} else {
-			isUnified, isUnifiedErr = st.Type == _cgroup2SuperMagic, nil
+			isUnified, isUnifiedErr = st.Type == unix.CGROUP2_SUPER_MAGIC, nil
 		}
 	})
 	return isUnified, isUnifiedErr
