containers/buildah

commit 9b299588c0936376ef9ccd7938f1cef27c831418

Rebuild layer if a change in ARG is detected
Check whether the ARG in the containerfile is changed by
either the --build-arg flag or local environment and use
the cached layer or rebuild the layer accordingly.
Add tests for this use case as well.

Signed-off-by: Urvashi Mohnani <umohnani@redhat.com>

imagebuildah/stage_executor.go
@@ -834,7 +834,11 @@ func (s *StageExecutor) Execute(ctx context.Context, base string) (imgID string,
 		// Check if there's already an image based on our parent that
 		// has the same change that we're about to make, so far as we
 		// can tell.
-		if checkForLayers {
+		// Only do this if there were no build args given by the user,
+		// we need to call ib.Run() to correctly put the args together before
+		// determining if a cached layer with the same build args already exists
+		// and that is done in the if block below.
+		if checkForLayers && s.builder.Args == nil {
 			cacheID, err = s.intermediateImageExists(ctx, node, addedContentSummary, s.stepRequiresLayer(step))
 			if err != nil {
 				return "", nil, errors.Wrap(err, "error checking if cached image exists from a previous build")
@@ -1022,6 +1026,9 @@ func (s *StageExecutor) getCreatedBy(node *parser.Node, addedContentSummary stri
 		return "/bin/sh"
 	}
 	switch strings.ToUpper(node.Value) {
+	case "ARG":
+		buildArgs := s.getBuildArgs()
+		return "/bin/sh -c #(nop) ARG " + buildArgs
 	case "RUN":
 		buildArgs := s.getBuildArgs()
 		if buildArgs != "" {

tests/bud.bats
@@ -303,17 +303,17 @@ symlink(subdir)"
   # two more, starting at the "echo $user" instruction
   run_buildah bud --signature-policy ${TESTSDIR}/policy.json --build-arg=user=1 --layers -t test1 -f Dockerfile.build-args ${TESTSDIR}/bud/use-layers
   run_buildah images -a
-  expect_line_count 7
+  expect_line_count 8
 
   # one more, because we added a new name to the same image
   run_buildah bud --signature-policy ${TESTSDIR}/policy.json --build-arg=user=1 --layers -t test2 -f Dockerfile.build-args ${TESTSDIR}/bud/use-layers
   run_buildah images -a
-  expect_line_count 8
+  expect_line_count 9
 
   # two more, starting at the "echo $user" instruction
   run_buildah bud --signature-policy ${TESTSDIR}/policy.json --layers -t test3 -f Dockerfile.build-args ${TESTSDIR}/bud/use-layers
   run_buildah images -a
-  expect_line_count 10
+  expect_line_count 12
 }
 
 @test "bud with --rm flag" {
@@ -1688,13 +1688,14 @@ _EOF
 }
 
 @test "bud-build-arg-cache" {
-  _prefetch busybox
+  _prefetch busybox alpine
   target=derived-image
   run_buildah bud --signature-policy ${TESTSDIR}/policy.json --layers -t ${target} -f Dockerfile3 ${TESTSDIR}/bud/build-arg
   run_buildah inspect -f '{{.FromImageID}}' ${target}
   targetid="$output"
 
-  # With build args, we should not find the previous build as a cached result.
+  # With build args, we should not find the previous build as a cached result. This will be true because there is a RUN command after all the ARG
+  # commands in the containerfile, so this does not truly test if the ARG commands were using cache or not. There is a test for that case below.
   run_buildah bud --signature-policy ${TESTSDIR}/policy.json --layers -t ${target} -f Dockerfile3 --build-arg=UID=17122 --build-arg=CODE=/copr/coprs_frontend --build-arg=USERNAME=praiskup --build-arg=PGDATA=/pgdata ${TESTSDIR}/bud/build-arg
   run_buildah inspect -f '{{.FromImageID}}' ${target}
   argsid="$output"
@@ -1716,6 +1717,30 @@ _EOF
   run_buildah bud --signature-policy ${TESTSDIR}/policy.json --layers -t ${target} -f Dockerfile3 --build-arg=PGDATA=/pgdata --build-arg=UID=17122 --build-arg=CODE=/copr/coprs_frontend --build-arg=USERNAME=praiskup ${TESTSDIR}/bud/build-arg
   run_buildah inspect -f '{{.FromImageID}}' ${target}
   expect_output "$argsid"
+
+  # If build-arg is specified via the command line and is different from the previous cached build, it should not use the cached layers.
+  # Note, this containerfile does not have any RUN commands and we verify that the ARG steps are being rebuilt when a change is detected.
+  run_buildah bud --signature-policy ${TESTSDIR}/policy.json --layers -t test-img -f Dockerfile4 ${TESTSDIR}/bud/build-arg
+  run_buildah inspect -f '{{.FromImageID}}' test-img
+  initialid="$output"
+
+  # Build the same containerfile again and verify that the cached layers were used
+  run_buildah bud --signature-policy ${TESTSDIR}/policy.json --layers -t test-img-1 -f Dockerfile4 ${TESTSDIR}/bud/build-arg
+  run_buildah inspect -f '{{.FromImageID}}' test-img-1
+  expect_output "$initialid"
+
+  # Set the build-arg flag and verify that the cached layers are not used
+  run_buildah bud --signature-policy ${TESTSDIR}/policy.json --layers -t test-img-2 --build-arg TEST=foo -f Dockerfile4 ${TESTSDIR}/bud/build-arg
+  run_buildah inspect -f '{{.FromImageID}}' test-img-2
+  argsid="$output"
+  [[ "$argsid" != "$initialid" ]]
+
+  # Set the build-arg via an ENV in the local environment and verify that the cached layers are not used
+  export TEST=bar
+  run_buildah bud --signature-policy ${TESTSDIR}/policy.json --layers -t test-img-3 --build-arg TEST -f Dockerfile4 ${TESTSDIR}/bud/build-arg
+  run_buildah inspect -f '{{.FromImageID}}' test-img-3
+  argsid="$output"
+  [[ "$argsid" != "$initialid" ]]
 }
 
 @test "bud test RUN with a priv'd command" {

tests/bud/build-arg/Dockerfile4
@@ -1,0 +1,3 @@
+FROM alpine
+ARG TEST
+ENV NAME=$TEST

commit ab8e351c60882abb0fef21b3ee7abe4df0d3d677

Fix caching layers with build args
This fixes a regression introduced in
9b299588c0936376ef9ccd7938f1cef27c831418.

ib.Run() is only really needed in the ARG step. On all the other steps,
it can cause potentially expensive commands to be executed unecessarily.

Closes https://github.com/containers/buildah/issues/2992

Signed-off-by: Antonio Terceiro <antonio.terceiro@linaro.org>
Signed-off-by: Urvashi Mohnani <umohnani@redhat.com>

imagebuildah/stage_executor.go
@@ -834,11 +834,12 @@ func (s *StageExecutor) Execute(ctx context.Context, base string) (imgID string,
 		// Check if there's already an image based on our parent that
 		// has the same change that we're about to make, so far as we
 		// can tell.
-		// Only do this if there were no build args given by the user,
+		// Only do this if the step we are on is not an ARG step,
 		// we need to call ib.Run() to correctly put the args together before
 		// determining if a cached layer with the same build args already exists
 		// and that is done in the if block below.
-		if checkForLayers && len(s.builder.Args) == 0 {
+		if checkForLayers && step.Command != "arg" {
+
 			cacheID, err = s.intermediateImageExists(ctx, node, addedContentSummary, s.stepRequiresLayer(step))
 			if err != nil {
 				return "", nil, errors.Wrap(err, "error checking if cached image exists from a previous build")

tests/bud.bats
@@ -300,7 +300,15 @@ symlink(subdir)"
   run_buildah images -a
   expect_line_count 5
 
-  # two more, starting at the "echo $user" instruction
+  # running the same build again does not run the commands again
+  run_buildah bud --signature-policy ${TESTSDIR}/policy.json --build-arg=user=0 --layers -t test -f Dockerfile.build-args ${TESTSDIR}/bud/use-layers
+  if [[ "$output" =~ "MAo=" ]]; then
+    # MAo= is the base64 of "0\n" (i.e. `echo 0`)
+    printf "Expected command not to run again if layer is cached\n" >&2
+    false
+  fi
+
+  # two more, starting at the "echo $user | base64" instruction
   run_buildah bud --signature-policy ${TESTSDIR}/policy.json --build-arg=user=1 --layers -t test1 -f Dockerfile.build-args ${TESTSDIR}/bud/use-layers
   run_buildah images -a
   expect_line_count 8
@@ -310,7 +318,7 @@ symlink(subdir)"
   run_buildah images -a
   expect_line_count 9
 
-  # two more, starting at the "echo $user" instruction
+  # two more, starting at the "echo $user | base64" instruction
   run_buildah bud --signature-policy ${TESTSDIR}/policy.json --layers -t test3 -f Dockerfile.build-args ${TESTSDIR}/bud/use-layers
   run_buildah images -a
   expect_line_count 12

tests/bud/use-layers/Dockerfile.build-args
@@ -1,4 +1,4 @@
 FROM alpine
 ARG user
-RUN echo $user
+RUN echo $user | base64
 RUN touch /tmp/hello