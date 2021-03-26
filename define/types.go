f66a6d660d5397b73cb7197cf1cd64e5153e7c29
Daniel J Walsh <dwalsh@redhat.com>
2021-03-26 07:53:56 -0400

    [NO TESTS NEEDED] Use --recurse-modules when building git context
    If a git repository contains submodules, we need to check them all
    out because the Containerfile/Dockerfile at the repo might use them.
    
    Fixes: https://github.com/containers/buildah/issues/3104
    
    It is too complicated to setup a recursive submodules git repo, so
    setting no tests needed.
    
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

--- define/types.go
+++ define/types.go
@@ -166,7 +166,7 @@ func cloneToDirectory(url, dir string) error {
 		cmd = exec.Command("git", "clone", url, dir)
 	} else {
 		logrus.Debugf("cloning repo %q and branch %q to %q", gitBranch[0], gitBranch[1], dir)
-		cmd = exec.Command("git", "clone", "-b", gitBranch[1], gitBranch[0], dir)
+		cmd = exec.Command("git", "clone", "--recurse-submodules", "-b", gitBranch[1], gitBranch[0], dir)
 	}
 	return cmd.Run()
 }
