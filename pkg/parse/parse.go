6adbfeeb4fc2cca89c6b71f393706c4def61d129
Aditya R <arajan@redhat.com>
2023-06-05 12:31:57 +0530

    GetTmpDir: honor image_copy_tmp_dir from containers.conf
    https://github.com/containers/common/blob/main/docs/containers.conf.5.md
    contains a field `image_copy_tmp_dir` so `GetTmpDir` must honor that in
    case `ENV: TMPDIR` is not found.
    
    Closes: https://github.com/containers/buildah/issues/4787
    
    Signed-off-by: Aditya R <arajan@redhat.com>

--- pkg/parse/parse.go
+++ pkg/parse/parse.go
@@ -999,6 +999,13 @@ func GetTempDir() string {
 	if tmpdir, ok := os.LookupEnv("TMPDIR"); ok {
 		return tmpdir
 	}
+	containerConfig, err := config.Default()
+	if err != nil {
+		tmpdir, err := containerConfig.ImageCopyTmpDir()
+		if err != nil {
+			return tmpdir
+		}
+	}
 	return "/var/tmp"
 }
 
