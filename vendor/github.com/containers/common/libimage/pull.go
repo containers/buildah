6ec6f046f5b73cb2aebea5cc09dfdfb4bcf92927
Valentin Rothberg <vrothberg@redhat.com>
2023-08-31 09:22:36 +0200

    vendor containers/common@12405381ff45
    When pulling from an OCI source, make sure to preseve the optional name.
    For instance, a podman pull oci:/tmp/foo:quay.io/foo/bar:latest should
    pull the image and name it quay.io/foo/bar:latest.
    
    While at it, also fix a bug when pulling an OCI without the optional
    name. Previously, we used the path to name the image which will error in
    most cases due to invalid characters (e.g., capital ones). Hence, apply
    the same trick as for the dir transport and generate a sha.
    
    Signed-off-by: Valentin Rothberg <vrothberg@redhat.com>

--- vendor/github.com/containers/common/libimage/pull.go
+++ vendor/github.com/containers/common/libimage/pull.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"os"
 	"runtime"
 	"strings"
 	"time"
@@ -229,8 +230,18 @@ func (r *Runtime) copyFromDefault(ctx context.Context, ref types.ImageReference,
 
 	case ociTransport.Transport.Name():
 		split := strings.SplitN(ref.StringWithinTransport(), ":", 2)
-		storageName = toLocalImageName(split[0])
-		imageName = storageName
+		if len(split) == 1 || split[1] == "" {
+			// Same trick as for the dir transport: we cannot use
+			// the path to a directory as the name.
+			storageName, err = getImageID(ctx, ref, nil)
+			if err != nil {
+				return nil, err
+			}
+			imageName = "sha256:" + storageName[1:]
+		} else { // If the OCI-reference includes an image reference, use it
+			storageName = split[1]
+			imageName = storageName
+		}
 
 	case ociArchiveTransport.Transport.Name():
 		manifestDescriptor, err := ociArchiveTransport.LoadManifestDescriptorWithContext(r.SystemContext(), ref)
@@ -592,6 +603,9 @@ func (r *Runtime) copySingleImageFromRegistry(ctx context.Context, imageName str
 		return nil
 	}
 
+	if socketPath, ok := os.LookupEnv("NOTIFY_SOCKET"); ok {
+		options.extendTimeoutSocket = socketPath
+	}
 	c, err := r.newCopier(&options.CopyOptions)
 	if err != nil {
 		return nil, err
