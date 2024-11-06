e5eec8b083597f6d155d5864aa0d4952c5daed06
Nalin Dahyabhai <nalin@redhat.com>
2024-11-06 17:22:49 -0500

    internal/volume.GetBindMount(): discard writes in bind mounts
    When handling RUN --mount=type=bind, where the mount is read-write,
    instead of a simple bind mount, create an overlay mount with an upper
    directory that will be discarded after the overlay mount is unmounted.
    This brings us in line with the expected behavior, wherein writes to
    bind mounts should be discarded.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- run_linux.go
+++ run_linux.go
@@ -386,7 +386,7 @@ rootless=%d
 	}
 
 	defer func() {
-		if err := b.cleanupRunMounts(options.SystemContext, mountPoint, runArtifacts); err != nil {
+		if err := b.cleanupRunMounts(mountPoint, runArtifacts); err != nil {
 			options.Logger.Errorf("unable to cleanup run mounts %v", err)
 		}
 	}()
@@ -1006,7 +1006,7 @@ func (b *Builder) runSetupVolumeMounts(mountLabel string, volumeMounts []string,
 				RootGID:                idMaps.rootGID,
 				UpperDirOptionFragment: upperDir,
 				WorkDirOptionFragment:  workDir,
-				GraphOpts:              b.store.GraphOptions(),
+				GraphOpts:              slices.Clone(b.store.GraphOptions()),
 			}
 
 			overlayMount, err := overlay.MountWithOptions(contentDir, host, container, &overlayOpts)
@@ -1015,7 +1015,7 @@ func (b *Builder) runSetupVolumeMounts(mountLabel string, volumeMounts []string,
 			}
 
 			// If chown true, add correct ownership to the overlay temp directories.
-			if foundU {
+			if err == nil && foundU {
 				if err := chown.ChangeHostPathOwnership(contentDir, true, idMaps.processUID, idMaps.processGID); err != nil {
 					return specs.Mount{}, err
 				}
