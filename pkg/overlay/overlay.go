6015a6929c996af9c769b178e22c10f78957b22c
Nalin Dahyabhai <nalin@redhat.com>
2025-01-13 17:53:37 -0500

    pkg/overlay: add a ForceMount flag to Options
    Add a ForceMount flag to pkg/overlay.Options that forces mounting the
    overlay filesystem and returning a bind mount to it instead of trying to
    leave that for later in cases where we're able to have the kernel do it.
    
    This is mainly for the sake of callers that want to do more things with
    the mounted overlay filesystem before passing them to the (presumably)
    OCI runtime.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>
    Signed-off-by: David Shea <dshea@redhat.com>

--- pkg/overlay/overlay.go
+++ pkg/overlay/overlay.go
@@ -11,6 +11,7 @@ import (
 	"errors"
 
 	"github.com/containers/storage/pkg/idtools"
+	"github.com/containers/storage/pkg/mount"
 	"github.com/containers/storage/pkg/system"
 	"github.com/containers/storage/pkg/unshare"
 	"github.com/opencontainers/runtime-spec/specs-go"
@@ -50,6 +51,10 @@ type Options struct {
 	RootUID int
 	// RootGID is not used yet but keeping it here for legacy reasons.
 	RootGID int
+	// Force overlay mounting and return a bind mount, rather than
+	// attempting to optimize by having the runtime actually mount and
+	// manage the overlay filesystem.
+	ForceMount bool
 }
 
 // TempDir generates an overlay Temp directory in the container content
@@ -146,6 +151,12 @@ func mountWithMountProgram(mountProgram, overlayOptions, mergeDir string) error 
 	return nil
 }
 
+// mountNatively mounts an overlay at mergeDir using the kernel's mount()
+// system call.
+func mountNatively(overlayOptions, mergeDir string) error {
+	return mount.Mount("overlay", mergeDir, "overlay", overlayOptions)
+}
+
 // MountWithOptions creates a subdir of the contentDir based on the source directory
 // from the source system.  It then mounts up the source directory on to the
 // generated mount point and returns the mount point to the caller.
@@ -210,6 +221,18 @@ func MountWithOptions(contentDir, source, dest string, opts *Options) (mount spe
 	mount.Destination = dest
 	mount.Type = "overlay"
 	mount.Options = strings.Split(overlayOptions, ",")
+
+	if opts.ForceMount {
+		if err := mountNatively(overlayOptions, mergeDir); err != nil {
+			return mount, err
+		}
+
+		mount.Source = mergeDir
+		mount.Destination = dest
+		mount.Type = "bind"
+		mount.Options = []string{"bind", "slave"}
+		return mount, nil
+	}
 
 	return mount, nil
 }
