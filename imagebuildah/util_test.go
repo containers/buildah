4383e34c0f3fab2d38f1ed72043b3213b8c57bd3
flouthoc <flouthoc.git@gmail.com>
2025-01-23 10:34:08 -0800

    build, run: record hash or digest in image history
    When using `--mount=type=bind` or `--mount=type=cache` the hash or
    digest of source in these flags should be added to image history so
    buildah can burst cache if files on host or image which is being used as
    source is changed.
    
    Signed-off-by: flouthoc <flouthoc.git@gmail.com>

---
+++ imagebuildah/util_test.go
@@ -0,0 +1,81 @@
+package imagebuildah
+
+import (
+	"archive/tar"
+	"io"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/opencontainers/go-digest"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGeneratePathChecksum(t *testing.T) {
+	tempDir := t.TempDir()
+
+	tempFile, err := os.CreateTemp(tempDir, "testfile")
+	if err != nil {
+		t.Fatalf("Failed to create temp file: %v", err)
+	}
+	defer tempFile.Close()
+
+	// Write some data to the file
+	data := []byte("Hello, world!")
+	if _, err := tempFile.Write(data); err != nil {
+		t.Fatalf("Failed to write data to temp file: %v", err)
+	}
+
+	// Generate the checksum for the directory
+	checksum, err := generatePathChecksum(tempDir)
+	if err != nil {
+		t.Fatalf("Failed to generate checksum: %v", err)
+	}
+
+	digester := digest.SHA256.Digester()
+	tarWriter := tar.NewWriter(digester.Hash())
+
+	err = filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		header, err := tar.FileInfoHeader(info, "")
+		if err != nil {
+			return err
+		}
+
+		relPath, err := filepath.Rel(tempDir, path)
+		if err != nil {
+			return err
+		}
+		header.Name = filepath.ToSlash(relPath)
+
+		if err := tarWriter.WriteHeader(header); err != nil {
+			return err
+		}
+
+		if !info.Mode().IsRegular() {
+			return nil
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		_, err = io.Copy(tarWriter, file)
+		return err
+	})
+	if err != nil {
+		tarWriter.Close()
+		t.Fatalf("Failed to manually generate checksum: %v", err)
+	}
+
+	tarWriter.Close()
+	expectedChecksum := digester.Digest().String()
+
+	// Compare the generated checksum to the expected checksum
+	assert.Equal(t, expectedChecksum, checksum, "didn't get expected checksum over a sample directory")
+}
