8cd79d4b7dd7de6c5ad0c8318a927144cc7df5ab
Nalin Dahyabhai <nalin@redhat.com>
2025-03-13 13:23:34 -0400

    RUN: only use an overlay for --mount=type=bind,rw on overlay
    Only use an overlay for read-write mounts of type=bind, where changes
    should be discarded, if the storage driver is overlay.  Otherwise, make
    a temporary copy and use a bind mount to the copy.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- imagebuildah/build_notlinux.go
+++ imagebuildah/build_notlinux.go
@@ -3,18 +3,95 @@
 package imagebuildah
 
 import (
+	"errors"
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/containers/buildah/copier"
 	"github.com/containers/buildah/define"
+	"github.com/containers/buildah/internal/tmpdir"
+	"github.com/sirupsen/logrus"
 	"go.podman.io/storage"
+	"go.podman.io/storage/pkg/idtools"
 )
 
-// platformSetupContextDirectoryOverlay() should set up an overlay _over_ the
-// build context directory, and sort out labeling.  Should return the location
-// which should be used as the default build context; the process label and
-// mount label for the build, if any; a boolean value that indicates whether we
-// did, in fact, mount an overlay; and a cleanup function which should be
-// called when the location is no longer needed (on success).  Returned errors
-// should be treated as fatal.
-// TODO: currenty a no-op on this platform.
+// platformSetupContextDirectoryOverlay() either creates a temporary copy of
+// the default build context directory.  Returns the location which should be
+// used as the default build context; an empty process label and mount label
+// for the build that makes more sense elsewhere; a boolean value that
+// indicates whether the caller can write directly to the location; and a
+// cleanup function which should be called when the location is no longer
+// needed (on success). Returned errors should be treated as fatal.
 func platformSetupContextDirectoryOverlay(store storage.Store, options *define.BuildOptions) (string, string, string, bool, func(), error) {
-	return options.ContextDirectory, "", "", false, func() {}, nil
+	var succeeded bool
+	var tmpDir string
+	cleanup := func() {
+		if tmpDir != "" {
+			if err := os.Remove(tmpDir); err != nil && !errors.Is(err, fs.ErrNotExist) {
+				logrus.Debugf("removing should-be-empty temporary directory %q: %v", tmpDir, err)
+			}
+		}
+	}
+	defer func() {
+		if !succeeded {
+			cleanup()
+		}
+	}()
+	// double-check that the context directory location is an absolute path
+	contextDirectoryAbsolute, err := filepath.Abs(options.ContextDirectory)
+	if err != nil {
+		return "", "", "", false, nil, fmt.Errorf("determining absolute path of %q: %w", options.ContextDirectory, err)
+	}
+	// create a temporary parent directory for the copy of the build context
+	tmpDir, err = os.MkdirTemp(tmpdir.GetTempDir(), "buildah-context-")
+	if err != nil {
+		return "", "", "", false, nil, fmt.Errorf("creating temporary directory: %w", err)
+	}
+	// create a subdirectory under it
+	tmpContextDir := filepath.Join(tmpDir, "build-context")
+	if err := os.Mkdir(tmpContextDir, 0o755); err != nil {
+		return "", "", "", false, nil, fmt.Errorf("creating a temporary directory under %s: %w", tmpDir, err)
+	}
+	// copy the contents of the default build context to the new location so that it can be written to more or less safely
+	getOptions := copier.GetOptions{
+		ChownDirs:  &idtools.IDPair{UID: 0, GID: 0},
+		ChownFiles: &idtools.IDPair{UID: 0, GID: 0},
+	}
+	var wg sync.WaitGroup
+	var putErr, getErr error
+	copyReader, copyWriter := io.Pipe()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		putErr = copier.Put(tmpContextDir, tmpContextDir, copier.PutOptions{}, copyReader)
+		copyReader.Close()
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		getErr = copier.Get(contextDirectoryAbsolute, contextDirectoryAbsolute, getOptions, []string{"."}, copyWriter)
+		copyWriter.Close()
+	}()
+	wg.Wait()
+	var errs []error
+	if getErr != nil {
+		errs = append(errs, getErr)
+	}
+	if putErr != nil {
+		errs = append(errs, putErr)
+	}
+	if len(errs) > 0 {
+		grouped := errs[0]
+		if len(errs) > 1 {
+			grouped = errors.Join(errs...)
+		}
+		return "", "", "", false, nil, fmt.Errorf("creating copy of build context directory: %w", grouped)
+	}
+	logrus.Debugf("created a copy of %q at %q", contextDirectoryAbsolute, tmpContextDir)
+	succeeded = true
+	return tmpContextDir, "", "", true, cleanup, nil
 }
