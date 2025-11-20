d329aacd0e467c73cdfb0e77111833995a919c1d
tomsweeneyredhat <tsweeney@redhat.com>
2025-11-19 19:58:20 -0500

    Bump runc to v1.2.9
    This addresses bumping crun to v1.2.9, which is a huge jump
    but is necessary to address CVE-2025-52881, CVE-2025-31133
    and CVE-2025-52565 plus various regressions in earlier versions.
    
    Fixes: https://issues.redhat.com/browse/RHEL-126919
    
    In order to both handle breaking changes related to removal of the
    unmaintained `github.com/docker/libnetwork` while keeping this branch as
    closely compatible as possible to `release-1.27`, the following major
    changes were necessary:
    
    - Removed duplicate functions from run_linux.go (setupMounts,
      runSetupRunMounts, getBindMount, getTmpfsMount, getSSHMount) and
      centralized them in run_common.go
    - Updated function signatures to use runMountInfo and IDMaps structs
      instead of individual parameters
    - Moved IDMaps and runMountInfo struct definitions from run_common.go
      to run.go to match 1.27 structure
    - Moved nonCleanablePrefixes variable from run.go to run_linux.go to
      match 1.27 structure
    - Updated addResolvConf call to use spec.Linux.Namespaces instead of
      namespaceOptions
    - Changed hardcoded "/etc/resolv.conf" strings to use
      resolvconf.DefaultResolvConf constant
    - Updated import aliases: run_common.go now uses imageTypes alias for
      github.com/containers/image/v5/types to match 1.27
    
    This consolidates mount-related code in run_common.go and aligns the
    1.26 branch structure with 1.27.
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- imagebuildah/executor.go
+++ imagebuildah/executor.go
@@ -167,7 +167,7 @@ func newExecutor(logger *logrus.Logger, logPrefix string, store storage.Store, o
 	}
 
 	devices := define.ContainerDevices{}
-	for _, device := range append(defaultContainerConfig.Containers.Devices, options.Devices...) {
+	for _, device := range append(defaultContainerConfig.Containers.Devices.Get(), options.Devices...) {
 		dev, err := parse.DeviceFromPath(device)
 		if err != nil {
 			return nil, err
@@ -176,7 +176,7 @@ func newExecutor(logger *logrus.Logger, logPrefix string, store storage.Store, o
 	}
 
 	transientMounts := []Mount{}
-	for _, volume := range append(defaultContainerConfig.Containers.Volumes, options.TransientMounts...) {
+	for _, volume := range append(defaultContainerConfig.Containers.Volumes.Get(), options.TransientMounts...) {
 		mount, err := parse.Volume(volume)
 		if err != nil {
 			return nil, err
