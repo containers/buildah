40acb7096bc5246e5543b7564dfb8ad333d635d1
Chris Evich <cevich@redhat.com>
2025-11-26 12:23:32 -0500

    Adjust supportability date to match RHEL 8.6
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- contrib/cirrus/setup.sh
+++ contrib/cirrus/setup.sh
@@ -10,7 +10,7 @@ source $(dirname $0)/lib.sh
 
 req_env_vars OS_RELEASE_ID OS_RELEASE_VER GOSRC IN_PODMAN_IMAGE
 
-_EOL=20230601
+_EOL=20260601
 if [[ $(date +%Y%m%d) -ge $_EOL ]]; then
     die "As of $_EOL this branch is probably
 no longer supported in RHEL 8.7/9.1, please
