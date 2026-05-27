30a4189415e5d0a3a0c5971307a6432d8ed1a097
Tom Sweeney <tsweeney@redhat.com>
2026-05-27 15:11:07 -0400

    Bump to Buildah v1.44.0
    Bump to Buildah v1.44.0 to go out with Podman v6.0
    
    Signed-off-by: Tom Sweeney <tsweeney@redhat.com>

--- define/types.go
+++ define/types.go
@@ -30,7 +30,7 @@ const (
 	// identify working containers.
 	Package = "buildah"
 	// Version for the Package. Also used by .packit.sh for Packit builds.
-	Version = "1.43.0-dev"
+	Version = "1.44.0"
 
 	// DefaultRuntime if containers.conf fails.
 	DefaultRuntime = "runc"
