74f93b98574f40668cf7df84b59e00c4af5936d8
Tom Sweeney <tsweeney@redhat.com>
2026-05-28 18:31:18 -0400

    Add changes to tests/serve/serve.go from 68b29e6
    Add changes to tests/serve/serve.go from https://github.com/containers/buildah/commit/68b29e64b8a8d03946de5588181736b9d60abc1a
    per suggestion from @nalind
    
    Signed-off-by: Tom Sweeney <tsweeney@redhat.com>

--- tests/serve/serve.go
+++ tests/serve/serve.go
@@ -10,7 +10,9 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"net/http/cgi"
 	"os"
+	"os/exec"
 	"path"
 	"path/filepath"
 	"strconv"
@@ -48,13 +50,46 @@ func sendThatFile(basepath string) func(w http.ResponseWriter, r *http.Request) 
 				if name.IsDir() {
 					suffix = "/"
 				}
-				builder.WriteString(fmt.Sprintf("<a href=%q>%s</a><br/>\n", path.Join(r.URL.Path, name.Name())+suffix, html.EscapeString(fs.FormatDirEntry(name))))
+				fmt.Fprintf(&builder, "<a href=%q>%s</a><br/>\n", path.Join(r.URL.Path, name.Name())+suffix, html.EscapeString(fs.FormatDirEntry(name)))
 			}
-			builder.WriteString("</html></body>")
+			fmt.Fprint(&builder, "</html></body>")
 			content = strings.NewReader(builder.String())
 		}
 		http.ServeContent(w, r, filename, finfo.ModTime(), content)
 	}
+}
+
+func runThatCGI(baseLocation, basePath, handlerAndEnvs string) func(w http.ResponseWriter, r *http.Request) {
+	handler, envList, _ := strings.Cut(handlerAndEnvs, ":")
+	handlerCmd := strings.Fields(handler) // github.com/mattn/go-shellwords.Parse() would be better, but we want to not require anything outside of the standard library, so this will have to do
+	if !filepath.IsAbs(handlerCmd[0]) {
+		abs, err := exec.LookPath(handlerCmd[0])
+		if err != nil {
+			log.Fatalf("finding %q: %v", handler, err)
+		}
+		handlerCmd[0] = abs
+	}
+	envSlice := strings.Split(envList, ":")
+	var envs, inherits []string
+	for _, env := range envSlice {
+		if strings.Contains(env, "=") {
+			envs = append(envs, env)
+		} else {
+			inherits = append(inherits, env)
+		}
+	}
+	cgiBackend := cgi.Handler{
+		Path:                handlerCmd[0],
+		Args:                handlerCmd[1:],
+		Root:                baseLocation,
+		Dir:                 basePath,
+		Env:                 envs,
+		InheritEnv:          inherits,
+		Stderr:              os.Stderr,
+		PathLocationHandler: http.DefaultServeMux,
+	}
+	log.Printf("cgi: %q -> %q started in %q %+v", baseLocation, handler, basePath, cgiBackend.Env)
+	return cgiBackend.ServeHTTP
 }
 
 func main() {
@@ -62,7 +97,7 @@ func main() {
 	if len(args) < 2 {
 		log.Fatal("requires subdirectory path [and optional port [and optional port file name [and optional TLS cert and key file names [and optional pid file name]]]]")
 	}
-	basedir := args[1]
+	spec := args[1]
 	port := "0"
 	if len(args) > 2 {
 		port = args[2]
@@ -78,7 +113,22 @@ func main() {
 			log.Fatalf("%v", err)
 		}
 	}
-	http.HandleFunc("/", sendThatFile(basedir))
+	roots := false
+	for specPart := range strings.SplitSeq(spec, ",") {
+		if content, subdirectoryAndHandlerAndEnvs, ok := strings.Cut(specPart, "="); ok {
+			if subdirectory, handlerAndEnvs, ok := strings.Cut(subdirectoryAndHandlerAndEnvs, ":"); ok {
+				http.HandleFunc(content, runThatCGI(content, subdirectory, handlerAndEnvs))
+			} else {
+				http.HandleFunc(content, sendThatFile(subdirectory))
+			}
+		} else {
+			if roots {
+				log.Fatal("only one content root allowed")
+			}
+			http.HandleFunc("/", sendThatFile(content))
+			roots = true
+		}
+	}
 	server := http.Server{
 		Addr: ":" + port,
 		BaseContext: func(l net.Listener) context.Context {
